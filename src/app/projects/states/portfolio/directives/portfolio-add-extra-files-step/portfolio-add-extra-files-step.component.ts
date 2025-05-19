dc337520f6253a541fede2c0904ec8a2d9ba9117
disururathnayake <disuru.office@gmail.com>
2025-05-19 13:19:20 +1000

    chore: create initial files for migration of portfolio-add-extra-files-step

---
+++ src/app/projects/states/portfolio/directives/portfolio-add-extra-files-step/portfolio-add-extra-files-step.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-add-extra-files-step',
+  templateUrl: 'portfolio-add-extra-files-step.component.html',
+  styleUrls: ['portfolio-add-extra-files-step.component.scss'],
+})
+export class PortfolioAddExtraFilesStep {
+  constructor() {}
+}
